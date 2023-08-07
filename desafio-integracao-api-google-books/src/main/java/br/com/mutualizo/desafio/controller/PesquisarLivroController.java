73c4bb1cf7eecc92e8d0a8249be485b10467fd93
Valdeir Nascimento <valdeircn11@gmail.com>
2023-08-07 00:15:03 -0300

    Alteração nos nomes dos projetos

--- desafio-integracao-api-google-books/src/main/java/br/com/mutualizo/desafio/controller/PesquisarLivroController.java
+++ desafio-integracao-api-google-books/src/main/java/br/com/mutualizo/desafio/controller/PesquisarLivroController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.constraints.NotBlank;
 import java.util.List;
 
 @RestController
@@ -22,7 +21,7 @@ public class PesquisarLivroController {
     }
 
     @GetMapping
-    public ResponseEntity<List<BookDTO>> pesquisar(@RequestParam @NotBlank String titulo) {
+    public ResponseEntity<List<BookDTO>> pesquisar(@RequestParam String titulo) {
         List<BookDTO> books = pesquisarLivroPorTitulo.pesquisar(titulo);
         return ResponseEntity.ok().body(books);
     }
